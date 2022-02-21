1dc075265cd219b3541f996d56b3428d934ab247
Harsheek Thanki <harsheekthanki@gmail.com>
2022-02-21 23:46:06 +0000

    all level 100 content displayed and extracted in console

--- script.js
+++ script.js
@@ -7,6 +7,32 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  // All episodes must be shown. For each episode, AT LEAST following must be displayed:
+  episodeList.forEach((episode) => {
+    // the episode's name
+    console.log(episode.name);
+
+    // Example: S02E07 would be the code for the 7th episode of the 2nd season. S2E7 would be incorrect.
+    if (episode.season >= 10 && episode.number >= 10) {
+      console.log(`S${episode.season}E${episode.number}`);
+    } else if (episode.season < 10 && episode.number >= 10) {
+      console.log(`S0${episode.season}E${episode.number}`);
+    } else if (episode.season >= 10 && episode.number < 10) {
+      console.log(`S${episode.season}E0${episode.number}`);
+    } else if (episode.season < 10 && episode.number < 10) {
+      console.log(`S0${episode.season}E0${episode.number}`);
+    }
+
+    // the episode's medium-sized image
+    console.log(episode.image.medium);
+
+    // the episode's summary text
+    console.log(episode.summary);
+  });
+  // Your page should state somewhere that the data has (originally) come from TVMaze.com, and link back to that site (or the specific episode on that site). See tvmaze.com/api#licensing.
+  console.log(`This content is from https://www.tvmaze.com/`);
+  console.log(`specifically: https://api.tvmaze.com/shows/82/episodes`);
 }
 
 window.onload = setup;
